73d59bf324f1d5d21bb4d5fbead41edee84eeaed
Usevalad <avm012@gmail.com>
2024-09-18 13:51:11 +0100

    18sep2024

---
+++ HerokuApp/src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class AddRemoveElementsTest extends BaseTest{
+    String deletebuttonLocator = "[onclick='deleteElement()']";
+
+    @Test
+    public void addRemoveElement(){
+
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addElement = driver.findElement(By.cssSelector("[onclick='addElement()']"));
+        addElement.click();
+        addElement.click();
+        List<WebElement> deleteButtons = driver.findElements(By.cssSelector(deletebuttonLocator));
+        assertEquals(deleteButtons.size(), 2, "Quantity of elements !=2");
+        deleteButtons.get(1).click();
+        deleteButtons = driver.findElements(By.cssSelector(deletebuttonLocator));
+        assertEquals(deleteButtons.size(), 1, "Quantity of elements !=1");
+    }
+}
---
+++ HerokuApp/src/test/java/FirstTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverInfo;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstTest extends BaseTest{
+
+    WebDriver driver;
+
+
+    @Test
+    public void firstTest() {
+        driver = new ChromeDriver();
+        driver.get("https://www.onliner.by/");
+        driver.quit();
+    }
+
+}
