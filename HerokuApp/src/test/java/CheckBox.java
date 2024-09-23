8979be9e2384d65e04efb3aa07695909b19928ab
Usevalad <avm012@gmail.com>
2024-09-23 12:08:54 +0100

    23sep2024

--- HerokuApp/src/test/java/CheckBox.java
+++ HerokuApp/src/test/java/CheckBox.java
@@ -2,13 +2,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class CheckBox extends BaseTest{
+public class CheckBox extends BaseTest {
     private static final String URL = "https://the-internet.herokuapp.com/checkboxes";
 
     @Test
-    public void isCheckBoxTwoSelected(){  //Should be - checkbox1 = unchecked, checkbox2 = checked.
+    public void isCheckBoxTwoSelected() {  //Should be - checkbox1 = unchecked, checkbox2 = checked.
         driver.get(URL);
 
         WebElement checkbox2 = driver.findElement(By.xpath("//*[@id='checkboxes']/input[2]"));
@@ -21,7 +24,7 @@ public class CheckBox extends BaseTest{
     }
 
     @Test
-    public void isCheckBoxOneSelected(){ //Should be - checkbox1 = checked, checkbox2 = unchecked.
+    public void isCheckBoxOneSelected() { //Should be - checkbox1 = checked, checkbox2 = unchecked.
         driver.get(URL);
 
         WebElement checkbox1 = driver.findElement(By.xpath("//*[@id='checkboxes']/input[1]"));
@@ -31,28 +34,24 @@ public class CheckBox extends BaseTest{
 
         boolean selected1 = checkbox1.isSelected();
         assertTrue(selected1, "Error");
-
         boolean selected2 = (!checkbox2.isSelected());
         assertTrue(selected2, "Error");
     }
 
+
     @Test
-    public void isCheckBoxOneSelectedV2(){ //Should be - checkbox1 = checked, checkbox2 = unchecked.
+    public void checkboxesShouldBeSelectedAndClickable() { //Should be - checkbox1 = checked, checkbox2 = unchecked.
         driver.get(URL);
 
-        WebElement checkbox1 = driver.findElement(By.xpath("//*[@id='checkboxes']/input[1]"));
-        //if (!checkbox1.isSelected()){
-            checkbox1.click();
-        //}
-        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id='checkboxes']/input[2]"));
-        //if (checkbox2.isSelected()){
-            checkbox2.click();
-        //}
+        List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@type='checkbox']"));
 
-        boolean selected1 = checkbox1.isSelected();
-        assertTrue(selected1, "Error");
+        assertFalse(checkboxes.get(0).isSelected());
+        assertTrue(checkboxes.get(1).isSelected());
 
-        boolean selected2 = (!checkbox2.isSelected());
-        assertTrue(selected2, "Error");
+        checkboxes.get(0).click();
+        checkboxes.get(1).click();
+
+        assertTrue(checkboxes.get(0).isSelected());
+        assertFalse(checkboxes.get(1).isSelected());
     }
 }
